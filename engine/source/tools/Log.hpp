c52b59916ada66006a1456e21ce820f78cdd5520
santi_dev <224229@buas.nl>
2024-05-25 00:43:13 +0200

    [Add] Logger (#13)

---
+++ engine/source/tools/Log.hpp
@@ -0,0 +1,64 @@
+
+#include <format>
+#include <iostream>
+
+namespace Log {
+
+enum class Severity {
+  NONE, // Applies no formatting or colours
+  INFO, // Use for debugging and general info
+  WARN, // Use when something is out of place, but still recoverable
+  ERROR // Use when crashing is what needs to happen
+};
+
+namespace detail {
+
+inline std::ostream *output = &std::cout;
+
+// Text Colours
+static constexpr auto green = "\033[32m";
+static constexpr auto yellow = "\033[33m";
+static constexpr auto red = "\033[31m";
+static constexpr auto reset = "\033[0m";
+
+// Type message
+inline const char *format_message_type(Severity type) {
+  switch (type) {
+  case Severity::INFO:
+    return "\033[32m"
+           "INFO: "
+           "\033[0m";
+  case Severity::WARN:
+    return "\033[33m"
+           "WARN:"
+           "\033[0m";
+  case Severity::ERROR:
+    return "\033[31m"
+           "ERROR:"
+           "\033[0m";
+  default:
+    return "";
+  }
+}
+
+inline std::string reduce_path(const std::string &in_path) {
+  return in_path.substr(in_path.find("KSEngine"));
+}
+
+} // namespace detail
+
+template <typename FormatString, typename... Args>
+inline void Message(Severity type, FormatString &&fmt, Args &&...args) {
+  *detail::output << detail::format_message_type(type)
+                  << std::vformat(fmt, std::make_format_args(
+                                           std::forward<Args>(args)...))
+                  << "\n"
+                  << "\n";
+}
+
+} // namespace Log
+
+#define LOG(severity, message, ...)                                            \
+  Log::Message(severity, " {} at Line {}: {}",                                 \
+               Log::detail::reduce_path(__FILE__), __LINE__, message,          \
+               __VA_ARGS__)
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <code_utility.hpp>
 #include <device/Device.hpp>
 #include <iostream>
+#include <tools/Log.hpp>
 
 int main() {
 
   KS::Device device = KS::Device(KS::DeviceInitParams{});
 
   std::string in;
-  while (in != std::string("exit")) { std::cin >> in; }
+  while (in != std::string("exit")) {
+    LOG(Log::Severity::ERROR, "Error here");
+    std::cin >> in;
+  }
 
   return 0;
 }
