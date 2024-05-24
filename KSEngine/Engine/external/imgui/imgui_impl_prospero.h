c4c153575f9d501ad21df0d74269c4190a7f2708
SantiagoBerna <224229@buas.nl>
2024-05-24 20:56:21 +0200

    Remove everything

--- KSEngine/Engine/external/imgui/imgui_impl_prospero.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "imgui.h"
-
-bool ImGui_Impl_Prospero_Init();
-void ImGui_Impl_Prospero_Shutdown();
-void ImGui_Impl_Prospero_NewFrame();
-void ImGui_Impl_Prospero_RenderDrawData(ImDrawData* data);
-
