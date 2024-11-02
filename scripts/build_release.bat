5f63fc7ed6701eded17c886c4b6d31bd7b02608a
Santiago Bernardino <santibberna@gmail.com>
2024-11-03 00:59:06 +0100

    [Removed] Unused scripts

--- scripts/build_release.bat
+++
@@ -1,7 +0,0 @@
-set Platform=Win64
-set Configuration=Release
-set OutDir="./build/%Platform%/%Configuration%"
-
-cmake -G Ninja -S "./" -B %OutDir% -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=%Configuration% -D PLATFORM=%Platform%
-cd %OutDir%
-ninja
