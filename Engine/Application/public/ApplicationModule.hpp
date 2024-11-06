8cc2b493b122ba6b1013bcd9252fc4efb6ea2b6d
Santiago Bernardino <santibberna@gmail.com>
2024-11-06 16:21:26 +0100

    [Add] Application Module

---
+++ Engine/Application/public/ApplicationModule.hpp
@@ -0,0 +1,46 @@
+#include <Device.hpp>
+#include <Engine.hpp>
+#include <ModuleInterface.hpp>
+#include <ecs/EntityComponentSystem.hpp>
+#include <fileio/ResourceHandle.hpp>
+#include <input/RawInput.hpp>
+#include <memory>
+#include <renderer/Renderer.hpp>
+#include <renderer/ShaderInputsBuilder.hpp>
+
+
+class Model;
+
+class ApplicationModule : public ModuleInterface
+{
+public:
+    virtual ~ApplicationModule() = default;
+
+private:
+    void Initialize(Engine& e) override;
+
+    void Shutdown(Engine& e) override
+    {
+    }
+
+    void UpdateWindow(Engine& e)
+    {
+        if (device->IsWindowOpen() == false)
+        {
+            e.SetExit(0);
+            return;
+        }
+
+        raw_input->ProcessInput();
+    }
+
+    void RenderFrame(Engine& e);
+
+    std::shared_ptr<Device> device {};
+    std::shared_ptr<EntityComponentSystem> ecs {};
+    std::shared_ptr<RawInput> raw_input {};
+    std::shared_ptr<ShaderInputs> mainInputs {};
+    std::shared_ptr<Renderer> renderer {};
+
+    ResourceHandle<Model> model {};
+};
