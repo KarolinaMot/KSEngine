6ebea932679fa3aa759cbc754aabb5f7d33aa927
Santiago Bernardino <santibberna@gmail.com>
2024-11-12 16:20:31 +0100

    [ReAdded] old files for reference

--- source/main.cpp
+++ source/main.cpp
@@ -24,6 +24,66 @@
 #include <resources/Model.hpp>
 #include <tools/Timer.hpp>
 
+KS::Camera FreeCamSystem(std::shared_ptr<KS::RawInput> input, entt::registry& registry, float dt)
+{
+    constexpr float MOUSE_SENSITIVITY = 0.003f;
+    constexpr float CAM_SPEED = 0.003f;
+
+    auto [x, y] = input->GetMouseDelta();
+    glm::vec3 eulerDelta {};
+
+    if (input->GetMouseButton(KS::MouseButton::Right) == KS::InputState::Pressed)
+    {
+        eulerDelta.y = x * MOUSE_SENSITIVITY;
+        eulerDelta.x = y * MOUSE_SENSITIVITY;
+        eulerDelta.x = glm::clamp(eulerDelta.x, -glm::radians(89.9f), glm::radians(89.9f));
+    }
+
+    glm::vec3 movement_dir {};
+    if (input->GetKeyboard(KS::KeyboardKey::W) == KS::InputState::Pressed)
+        movement_dir += KS::World::FORWARD;
+
+    if (input->GetKeyboard(KS::KeyboardKey::S) == KS::InputState::Pressed)
+        movement_dir -= KS::World::FORWARD;
+
+    if (input->GetKeyboard(KS::KeyboardKey::D) == KS::InputState::Pressed)
+        movement_dir += KS::World::RIGHT;
+
+    if (input->GetKeyboard(KS::KeyboardKey::A) == KS::InputState::Pressed)
+        movement_dir -= KS::World::RIGHT;
+
+    if (input->GetKeyboard(KS::KeyboardKey::E) == KS::InputState::Pressed)
+        movement_dir += KS::World::UP;
+
+    if (input->GetKeyboard(KS::KeyboardKey::Q) == KS::InputState::Pressed)
+        movement_dir -= KS::World::UP;
+
+    if (glm::length(movement_dir) != 0.0f)
+    {
+        movement_dir = glm::normalize(movement_dir);
+    }
+
+    auto view = registry.view<KS::ComponentFirstPersonCamera, KS::ComponentTransform>();
+    for (auto&& [e, camera, transform] : view.each())
+    {
+
+        camera.eulerAngles += eulerDelta;
+        camera.eulerAngles.x = glm::clamp(camera.eulerAngles.x, -glm::radians(89.9f), glm::radians(89.9f));
+
+        // LOG(Log::Severity::INFO, "{} {}", camera.eulerAngles.x, camera.eulerAngles.y);
+
+        auto rotation = glm::quat(camera.eulerAngles);
+        auto translation = transform.GetLocalTranslation();
+
+        transform.SetLocalTranslation(translation + rotation * (movement_dir * dt * CAM_SPEED));
+        transform.SetLocalRotation(rotation);
+
+        return camera.GenerateCamera(transform.GetWorldMatrix());
+    }
+
+    return KS::Camera {};
+}
+
 int main()
 {
     auto model = KS::ModelImporter::ImportFromFile("assets/models/DamagedHelmet.glb").value();
@@ -65,7 +125,6 @@ int main()
 
     std::string shaderPath = "assets/shaders/Deferred.hlsl";
     KS::Formats formats[4] = { KS::Formats::R32G32B32A32_FLOAT, KS::Formats::R8G8B8A8_UNORM, KS::Formats::R8G8B8A8_UNORM, KS::Formats::R8G8B8A8_UNORM };
-
     std::shared_ptr<KS::Shader> mainShader = std::make_shared<KS::Shader>(*device,
         KS::ShaderType::ST_MESH_RENDER,
         mainInputs,
---
+++ source/renderer/Shader.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <renderer/InfoStructs.hpp>
+
+namespace KS
+{
+class Device;
+class ShaderInputs;
+
+enum class ShaderType
+{
+    ST_RT_MESH_RENDER,
+
+    ST_MESH_RENDER,
+
+    ST_COMPUTE
+};
+class Shader
+{
+public:
+    Shader(const Device& device, ShaderType shaderType, std::shared_ptr<ShaderInputs> shaderInput, std::string path, Formats* rtFormats = nullptr, int numFormats = 1);
+    Shader(const Device& device, ShaderType shaderType, void* shaderInput, std::string path);
+    ~Shader();
+    std::shared_ptr<ShaderInputs> GetShaderInput() const { return m_shader_input; };
+    void* GetPipeline() const;
+    ShaderType GetShaderType() { return m_shader_type; }
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> m_impl;
+    std::shared_ptr<ShaderInputs> m_shader_input;
+    ShaderType m_shader_type;
+};
+}
