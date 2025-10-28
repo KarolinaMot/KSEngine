9d1e7f5739b32916e67a8a30df2ac13dbde24d16
Karolina Motužytė <224672@buas.nl>
2025-10-28 13:23:56 +0100

    Fixed cubemap rendering

--- assets/shaders/RenderCubemap.hlsl
+++ assets/shaders/RenderCubemap.hlsl
@@ -37,5 +37,5 @@ PS_INPUT mainVS(VS_INPUT input)
 
 float4 mainPS(PS_INPUT input) : SV_TARGET
 {
-    return float4(skyMap.Sample(samplr, input.vertexPos.xyz).rgb, 1.f);
+    return float4(skyMap.SampleLevel(samplr, input.vertexPos.xyz, 0.f).rgb, 1.f);
 }
